1fe635ed7f255170e121c86d952b50a6579ced8d
cagatay <cagatayyuksel@gmail.com>
2011-06-07 20:54:30 +0300

    changed url, fixes empty playlist with many moods

--- stereomood.py
+++ stereomood.py
@@ -6,20 +6,25 @@ import json
 import os
 
 player_command = 'mpg123 --list {0}'
-stereomood_url = 'http://stereomood.com/activity/{0}/playlist.json'
+stereomood_url = 'http://stereomood.com/mood/{0}/playlist.json?save'
+referer = 'http://stereomood.com/mood/{0}'
 pl_file_name = '/tmp/{0}.pl'
 songs = []
 
 try:
     mood = sys.argv[1]
     stereomood_url = stereomood_url.format(mood)
+    referer = referer.format(mood)
     pl_file_name = pl_file_name.format(mood)
     player_command = player_command.format(pl_file_name)
 except:
     print('usage: %s mood'%sys.argv[0])
     exit(1)
 
+req = request.Request(stereomood_url)
+req.add_header('Referer', referer)
 response = request.urlopen(stereomood_url).read().decode()
+import pdb;pdb.set_trace()
 playlist = json.loads(response)
 
 pl_file = open(pl_file_name, 'w')
