60a9c7f07a9d16227e81dc0714f7ea2e87ffa5d4
cagatay <cagatayyuksel@gmail.com>
2011-06-07 21:24:31 +0300

    removed debug statements, now fetches the whole playlist

--- stereomood.py
+++ stereomood.py
@@ -5,34 +5,47 @@ import sys
 import json
 import os
 
-player_command = 'mpg123 --list {0}'
-stereomood_url = 'http://stereomood.com/mood/{0}/playlist.json?save'
+player_command_raw = 'mpg123 --list {0}'
+stereomood_url_raw = 'http://stereomood.com/mood/{0}/playlist.json?save&index={1}'
 referer = 'http://stereomood.com/mood/{0}'
-pl_file_name = '/tmp/{0}.pl'
+pl_file_name_raw = '/tmp/{0}_{1}.pl'
 songs = []
 
 try:
     mood = sys.argv[1]
-    stereomood_url = stereomood_url.format(mood)
     referer = referer.format(mood)
-    pl_file_name = pl_file_name.format(mood)
-    player_command = player_command.format(pl_file_name)
 except:
     print('usage: %s mood'%sys.argv[0])
     exit(1)
 
+stereomood_url = stereomood_url_raw.format(mood, 1)
+
+# get number of tracks
+# this request may be redundant
+# but i'm too lazy to fix this now
 req = request.Request(stereomood_url)
 req.add_header('Referer', referer)
 response = request.urlopen(stereomood_url).read().decode()
-import pdb;pdb.set_trace()
+
 playlist = json.loads(response)
+total = int(playlist['tracksTotal'])
+count = round(total/20) + 1
 
-pl_file = open(pl_file_name, 'w')
+for i in range(1, count):
+    stereomood_url = stereomood_url_raw.format(mood, i)
+    pl_file_name = pl_file_name_raw.format(mood, i)
+    player_command = player_command_raw.format(pl_file_name)
 
-for song in playlist['trackList']:
-    songs.append(song['location'])
+    req = request.Request(stereomood_url)
+    req.add_header('Referer', referer)
+    response = request.urlopen(stereomood_url).read().decode()
+    playlist = json.loads(response)
 
-pl_file.write('\n'.join(songs))
-pl_file.close()
+    for song in playlist['trackList']:
+        songs.append(song['location'])
 
-os.system(player_command)
+    pl_file = open(pl_file_name, 'w')
+    pl_file.write('\n'.join(songs))
+    pl_file.close()
+
+    os.system(player_command)
